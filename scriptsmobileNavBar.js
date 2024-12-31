87481f391d44aeb10204af8e7d608f501ee59038
Malcolm <buildingwithkieth@outlook.com>
2024-12-30 20:07:16 -0600

    Messing with the mobile navBar

---
+++ scriptsmobileNavBar.js
@@ -0,0 +1,12 @@
+/*function toggleMenu() {
+    var menu = document.getElementById('fullscreen-menu');
+    menu.classList.toggle('open');
+}
+*/
+function openNav() {
+    document.getElementById("mNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mNav").style.width = "0%";
+}
